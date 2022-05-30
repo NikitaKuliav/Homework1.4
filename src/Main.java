512b24b0d7cf6123d17b517a4324f9fd239e941a
Nikita <athlon64x2600@gmail.com>
2022-05-30 17:20:21 +0200

    // Задача 1. Объявите три массива.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+
+        int[] animals = new int[3];
+        animals[0] = 1;
+        animals[1] = 2;
+        animals[2] = 3;
+
+        double[] weights = {1.57, 7.654, 9.986};
+
+        int[] birds = {3, 5, 7, 9};
+
+
+
     }
 }
